0bdfd221fb747b739874d4f244a27eeace50cae9
rohanshrestha09 <shresthacr11@gmail.com>
2022-09-01 11:10:40 +0545

    re:Fetch on Windows Focus:false

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,22 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <QueryClientProvider client={new QueryClient()}>
+  <QueryClientProvider
+    client={
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+    }
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
