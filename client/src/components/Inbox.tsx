bffa02bfaa13444305aa157e6c5e53469656932f
rohanshrestha09 <shresthacr11@gmail.com>
2022-09-08 10:30:02 +0545

    React Query Refactor

--- client/src/components/Inbox.tsx
+++ client/src/components/Inbox.tsx
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { io } from "socket.io-client";
-import { Avatar, Dropdown, Spinner } from "flowbite-react";
-import { IoIosArrowBack, IoIosCall, IoMdSend } from "react-icons/io";
-import { BiDotsVerticalRounded } from "react-icons/bi";
-import { BsEmojiHeartEyesFill } from "react-icons/bs";
-import userContext from "../utils/userContext";
-import { getMessage, getUserById, sendMessage } from "../api";
-import emojiArr from "./emoji";
+import React, { useContext, useEffect, useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { io } from 'socket.io-client';
+import { Avatar, Dropdown, Spinner } from 'flowbite-react';
+import { IoIosArrowBack, IoIosCall, IoMdSend } from 'react-icons/io';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { BsEmojiHeartEyesFill } from 'react-icons/bs';
+import userContext from '../utils/userContext';
+import { getMessage, getUserById, sendMessage } from '../api';
+import emojiArr from './emoji';
 
 interface Props {
   inboxToggle: boolean;
@@ -20,24 +20,19 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
 
   const { user } = useContext<any>(userContext);
 
-  const [realTimeMessage, setRealTimeMessage] = useState<
-    { self: boolean; message: string }[]
-  >([]);
+  const [realTimeMessage, setRealTimeMessage] = useState<{ self: boolean; message: string }[]>([]);
 
   const [hideEmoji, setHideEmoji] = useState<boolean>(true);
 
-  const [message, setMessage] = useState<string>("");
+  const [message, setMessage] = useState<string>('');
 
   useEffect(() => {
     socket &&
       socket.on(
-        "receive-message",
+        'receive-message',
         ({ from, to, msg }: { from: string; to: string; msg: string }) => {
           if (from === getId && to === user._id)
-            setRealTimeMessage((message: any) => [
-              { self: false, message: msg },
-              ...message,
-            ]);
+            setRealTimeMessage((message: any) => [{ self: false, message: msg }, ...message]);
         }
       );
     // eslint-disable-next-line
@@ -48,21 +43,19 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
     setMessage(value);
   };
 
-  const {
-    data: messageData,
-    isFetching: isMessageFetching,
-    isSuccess: isMessageSuccess,
-  } = useQuery({
+  const { isFetching: isMessageFetching } = useQuery({
     queryFn: () => getMessage({ from: user && user._id, to: getId }),
-    queryKey: ["getMessage", getId],
+    queryKey: ['getMessage', getId],
     enabled: !!getId,
-    refetchOnWindowFocus: false,
-    onSuccess: () => setSocket(io("/")),
+    onSuccess: (data) => {
+      setSocket(io('/'));
+      setRealTimeMessage(data);
+    },
   });
 
   const { data: receiverProfile } = useQuery({
     queryFn: () => getUserById(getId),
-    queryKey: ["getUserById", getId],
+    queryKey: ['getUserById', getId],
     enabled: !!getId,
   });
 
@@ -71,34 +64,30 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
     {
       onSuccess: (data) => {
         socket &&
-          socket.emit("send-message", {
+          socket.emit('send-message', {
             from: user && user._id,
             to: getId,
             msg: message,
           });
-        setRealTimeMessage((el: any) => [{ self: true, message }, ...el]);
-        setMessage("");
+        setRealTimeMessage((prev: any) => [{ self: true, message }, ...prev]);
+        setMessage('');
       },
     }
   );
 
-  useEffect(() => {
-    if (isMessageSuccess) setRealTimeMessage(messageData);
-  }, [isMessageSuccess, messageData]);
-
   return (
     <div
       className={`${
-        inboxToggle ? "flex" : "hidden"
+        inboxToggle ? 'flex' : 'hidden'
       } h-full md:col-span-4 col-span-full md:flex flex-col justify-between drop-shadow-lg rounded-lg shadow-lg shadow-slate-400`}
     >
-      <div className="w-full basis-20 flex justify-between items-center rounded-tl-lg px-6 shadow-lg">
+      <div className='w-full basis-20 flex justify-between items-center rounded-tl-lg px-6 shadow-lg'>
         <IoIosArrowBack
           size={25}
-          className="text-slate-700 cursor-pointer"
+          className='text-slate-700 cursor-pointer'
           onClick={(): void => setInboxToggle(false)}
         />
-        <div className="basis-3/4">
+        <div className='basis-3/4'>
           <Avatar
             img={
               receiverProfile && receiverProfile.imgUrl
@@ -106,50 +95,41 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
                 : undefined
             }
             rounded={true}
-            status="online"
-            statusPosition="bottom-right"
+            status='online'
+            statusPosition='bottom-right'
           >
-            <div className="space-y-1 font-medium">
-              <div className="truncate">
-                {receiverProfile && receiverProfile.fullname}
-              </div>
-              <div className="text-sm text-gray-500 truncate">Active Now</div>
+            <div className='space-y-1 font-medium'>
+              <div className='truncate'>{receiverProfile && receiverProfile.fullname}</div>
+              <div className='text-sm text-gray-500 truncate'>Active Now</div>
             </div>
           </Avatar>
         </div>
-        <IoIosCall size={25} className="text-slate-700 mx-3" />
-        <div className="flex items-center justify-center">
+        <IoIosCall size={25} className='text-slate-700 mx-3' />
+        <div className='flex items-center justify-center'>
           <BiDotsVerticalRounded
             size={25}
-            className="text-slate-700 absolute -z-10 pointer-events-none"
+            className='text-slate-700 absolute -z-10 pointer-events-none'
           />
-          <div className="dropdown dropdown-end text-white">
+          <div className='dropdown dropdown-end text-white'>
             <label
               tabIndex={0}
-              className="btn bg-transparent outline-none border-0 hover:bg-transparent"
+              className='btn bg-transparent outline-none border-0 hover:bg-transparent'
             ></label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu p-2 shadow rounded-box w-52 bg-white"
-            >
+            <ul tabIndex={0} className='dropdown-content menu p-2 shadow rounded-box w-52 bg-white'>
               <Dropdown.Item>Delete Conversation</Dropdown.Item>
             </ul>
           </div>
         </div>
       </div>
-      <div className="w-full basis-3/4 flex flex-col-reverse px-6 overflow-scroll overflow-x-hidden">
+      <div className='w-full basis-3/4 flex flex-col-reverse px-6 overflow-scroll overflow-x-hidden'>
         {isMessageFetching ? (
-          <Spinner color="pink" aria-label="Pink spinner example" size={"xl"} />
+          <Spinner color='pink' aria-label='Pink spinner example' size={'xl'} />
         ) : (
           realTimeMessage &&
           realTimeMessage.map((element: any, index: number) => (
-            <div className="w-full flex flex-col" key={index}>
-              <div
-                className={`items-end mt-3 ${
-                  !element.self ? "flex" : "hidden"
-                }`}
-              >
-                <div className="mr-2">
+            <div className='w-full flex flex-col' key={index}>
+              <div className={`items-end mt-3 ${!element.self ? 'flex' : 'hidden'}`}>
+                <div className='mr-2'>
                   <Avatar
                     img={
                       receiverProfile && receiverProfile.imgUrl
@@ -157,16 +137,16 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
                         : undefined
                     }
                     rounded={true}
-                    size="xs"
+                    size='xs'
                   ></Avatar>
                 </div>
-                <div className="min-h-fit md:max-w-[45%] max-w-[65%] border bg-slate-50 py-[14px] px-[18px] rounded-3xl rounded-bl-none md:text-sm text-xs break-words">
+                <div className='min-h-fit md:max-w-[45%] max-w-[65%] border bg-slate-50 py-[14px] px-[18px] rounded-3xl rounded-bl-none md:text-sm text-xs break-words'>
                   {element.message}
                 </div>
               </div>
               <div
                 className={`${
-                  element.self ? "block" : "hidden"
+                  element.self ? 'block' : 'hidden'
                 } min-h-fit md:max-w-[45%] max-w-[65%] mt-3 bg-[#1a2238] text-white py-[14px] px-[18px] rounded-3xl rounded-br-none md:text-sm text-xs self-end break-words block`}
               >
                 {element.message}
@@ -176,9 +156,9 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
         )}
       </div>
 
-      <div className="w-full basis-24 flex items-center justify-center px-6">
+      <div className='w-full basis-24 flex items-center justify-center px-6'>
         <form
-          className="w-full h-12 flex items-center justify-between px-4 rounded-full border border-slate-400"
+          className='w-full h-12 flex items-center justify-between px-4 rounded-full border border-slate-400'
           onSubmit={(event: React.SyntheticEvent) => {
             if (message) {
               event.preventDefault();
@@ -192,12 +172,12 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
         >
           <div
             className={`absolute max-h-72 overflow-scroll overflow-x-hidden bottom-20 ${
-              hideEmoji ? "hidden" : "grid"
+              hideEmoji ? 'hidden' : 'grid'
             } grid-rows-auto grid-cols-6 p-1 rounded-2xl rounded-bl-none bg-slate-50 list-none`}
           >
             {emojiArr.map((element, index) => (
               <li
-                className="text-3xl cursor-pointer"
+                className='text-3xl cursor-pointer'
                 key={index}
                 onClick={(): void => setMessage(message.concat(element))}
               >
@@ -208,21 +188,21 @@ const Inbox: React.FC<Props> = ({ inboxToggle, setInboxToggle, getId }) => {
 
           <BsEmojiHeartEyesFill
             size={28}
-            className="text-slate-500 cursor-pointer"
+            className='text-slate-500 cursor-pointer'
             onClick={(): void => setHideEmoji(!hideEmoji)}
           />
 
           <input
-            placeholder="Message"
+            placeholder='Message'
             value={message}
-            className="w-full h-full border-0 bg-inherit focus:border-0 focus:outline-none px-5"
+            className='w-full h-full border-0 bg-inherit focus:border-0 focus:outline-none px-5'
             onChange={onChange}
           />
 
           <IoMdSend
             size={28}
             className={`${
-              handleSendMessage.isLoading && "pointer-events-none"
+              handleSendMessage.isLoading && 'pointer-events-none'
             } cursor-pointer text-[#1a2238]`}
             onClick={(event: React.SyntheticEvent) => {
               if (message) {
